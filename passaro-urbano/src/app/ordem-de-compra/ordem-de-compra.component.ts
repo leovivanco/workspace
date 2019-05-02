ee964654adcb08be914e05e4e785be01a8177fc2
Leonardo Vivanco <leorvivanco@gmail.com>
2019-05-01 23:57:58 -0300

    Add: Validators FormGroup

--- passaro-urbano/src/app/ordem-de-compra/ordem-de-compra.component.ts
+++ passaro-urbano/src/app/ordem-de-compra/ordem-de-compra.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OrdemCompraService } from '../ordem-compra.service'
 import { Pedido } from '../pedido.model';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 
 @Component({
@@ -14,18 +14,24 @@ export class OrdemCompraComponent implements OnInit {
 
   constructor(private ordemCompraService: OrdemCompraService) { }
   idPedido: number;
+  //form.get('endereco').valid && formulario.get(endereco)
   form: FormGroup = new FormGroup({
-    'endereco': new FormControl(null),
-    'numero': new FormControl(null),
+    'endereco': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(200)]),
+    'numero': new FormControl(null, [Validators.required, Validators.minLength(1), Validators.maxLength(40)]),
     'complemento': new FormControl(null),
-    'pagamento': new FormControl(null)
+    'pagamento': new FormControl(null, [Validators.required])
   })
 
   ngOnInit(){
 
   }
   confirmarPagamento():void{
-    console.log(this.form)
+    if (this.form.status === "INVALID") {
+      this.form.get('endereco').markAsTouched()
+      this.form.get('numero').markAsTouched()
+      this.form.get('pagamento').markAsTouched()
+      this.form.get('complemento').markAsTouched()
+    }
   }
 
 
